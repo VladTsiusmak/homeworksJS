36583ce154b65739b5beec879c75eb5338514e95
VladTsiusmak <tsuva01032005@gmail.com>
2025-07-31 17:40:19 +0300

    домашня робота №1

---
+++ Lesson1/main.js
@@ -0,0 +1,35 @@
+{// #dYQNrBV
+    let a = 'hello';
+    let b = 'owu';
+    let c = 'com';
+    let d ='ua';
+    let e = 1;
+    let f = 10;
+    let g = -999;
+    let h = 123;
+    let i = 3.14;
+    let j = 2.7;
+    let k = 16;
+    let l = true;
+    let m = false;
+
+    console.log(a,b,c,d,e,f,g,h,i,j,k,l,m);
+}
+
+{// #6Qb97gsv
+    let firstName;
+    firstName = 'Владислав';
+    let midleName;
+    midleName = 'Анатолійович';
+    let lastName;
+    lastName = 'Цюсьмак';
+    let person = firstName + ' ' + midleName + ' ' + lastName;
+    console.log(person);
+}
+
+{// #4N0y5tufA
+    let a = 100;
+    let b = '100';
+    let c = true;
+    console.log(typeof a, typeof b, typeof c);
+}
