93777893232a0545d51a08c7d849f753e325999e
VladTsiusmak <tsuva01032005@gmail.com>
2025-08-01 17:24:27 +0300

    домашня робота №2

---
+++ Lesson2/task12/main.js
@@ -0,0 +1,19 @@
+// #awLXL6TBzg
+// з файлу arrays.js (лежить у вкладеннях) взяти масив coursesAndDurationArray. За допомогою іф перевірити кожен
+// його елемент на тривалість навчання. У випадку, якщо тривалість довша за 5 місяців, вивести в консоль “Супер”.
+console.log(coursesAndDurationArray);
+if (coursesAndDurationArray[0].monthDuration >= 5) {
+    console.log('Супер')
+}
+if (coursesAndDurationArray[1].monthDuration >= 5) {
+    console.log('Супер')
+}
+if (coursesAndDurationArray[2].monthDuration >= 5) {
+    console.log('Супер')
+}
+if (coursesAndDurationArray[3].monthDuration >= 5) {
+    console.log('Супер')
+}
+if (coursesAndDurationArray[4].monthDuration >= 5) {
+    console.log('Супер')
+}
